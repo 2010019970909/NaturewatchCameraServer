462780a3a30bd716c61f00854e0645968a3a8ab6
Vincent STRAGIER <vincent.stragier97@gmail.com>
2023-07-03 12:00:16 +0200

    Improve debug messages and display them directly on the webpage.

--- naturewatch_camera_server/__main__.py
+++ naturewatch_camera_server/__main__.py
@@ -1,11 +1,11 @@
-"""Main entry point for the NatureWatch Camera Server."""
 import argparse
-import subprocess
-
-from picamera.exc import PiCameraError  # pylint: disable=import-error
+# import subprocess
+import traceback
 
 from naturewatch_camera_server import create_app, create_error_app
 
+# Todo: intantiate the logger here, so that it can be used here and in the app.
+# probably need to pass it to the app and to fetch the config for the log file path.
 parser = argparse.ArgumentParser(
     description='Launch My Naturewatch Camera'
 )
@@ -31,22 +31,18 @@ class CameraNotFoundException(Exception):
         super().__init__(self.message)
 
 
-def is_camera_enabled():
-    """Check if the camera is enabled in raspi-config.
-    :return: True if the camera is enabled, False otherwise.
-    :rtype: bool
-    """
-    # inspired by https://stackoverflow.com/questions/58250817/raspberry-pi-camera-module-check-if-connected#comment102874971_58250817 # pylint: disable=line-too-long
-    vcgencmd_result = subprocess.run(
-        ['vcgencmd', 'get_camera'], stdout=subprocess.PIPE, check=False)
-    result_text = vcgencmd_result.stdout.decode('utf-8').strip()
-    # Split parameters and remove unwanted coma
-    result_text = result_text.replace(',', '').split(' ')
-    # Only keep parameters with equal sign
-    result_text = [parameter for parameter in result_text if '=' in parameter]
+# def is_camera_enabled():
+#     # inspired by https://stackoverflow.com/questions/58250817/raspberry-pi-camera-module-check-if-connected#comment102874971_58250817
+#     vcgencmd_result = subprocess.run(
+#         ['vcgencmd', 'get_camera'], stdout=subprocess.PIPE)
+#     result_text = vcgencmd_result.stdout.decode('utf-8').strip()
+#     # Split parameters and remove unwanted coma
+#     result_text = result_text.replace(',', '').split(' ')
+#     # Only keep parameters with equal sign
+#     result_text = [parameter for parameter in result_text if '=' in parameter]
 
-    properties = dict(pair.split('=') for pair in result_text)
-    return properties['supported'] == '1'
+#     properties = dict(pair.split('=') for pair in result_text)
+#     return properties['supported'] == '1'
 
 
 if __name__ == '__main__':
@@ -55,15 +51,17 @@ if __name__ == '__main__':
         app.camera_controller.start()
         app.change_detector.start()
 
-    except PiCameraError as error:
-        if "Camera is not enabled" in str(error):
-            # This error message appears even if the camera _is_ enabled,
-            # but the camera is not found,e.g., due to a connection problem.
-            # We don't want to mislead users into messing with raspi-config,
-            # so check if the camera interface is really disabled.
-            if is_camera_enabled():
-                error = CameraNotFoundException(
-                    "Unable to access camera. Is the cable properly connected?")
+    except Exception as error:
+        # if "Camera is not enabled" in str(error):
+        # This error message appears even if the camera _is_ enabled, but the camera is not found.
+        # e.g. due to a connection problem.
+        # We don't want to mislead users into messing with raspi-config, so check if the
+        # camera interface is really disabled.
+        # if (is_camera_enabled()):
+        #     error = CameraNotFoundException(
+        #         "Unable to access camera. Is the cable properly connected?")
+        error = traceback.format_exc()
+        print(f'{error = }')
 
         app = create_error_app(error)
 
